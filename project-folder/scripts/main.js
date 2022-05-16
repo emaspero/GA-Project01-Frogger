2f40003d00162960b6c9092f5febb8cf5ba64284
emaspero <emaspero@hotmail.com>
2022-05-16 11:47:09 +0100

    Initialising Repo

---
+++ project-folder/scripts/main.js
@@ -0,0 +1,5 @@
+const timeLeft = document.getElementById("#timeLeft");
+const result = document.getElementById("#result");
+const startPauseBtn = document.getElementById("#startPauseBtn");
+const cells = document.querySelectorAll(".grid div");
+
